f92e734430c862b3ec6d6bf148afe0dedd4601b1
weaq <30413309+weaq@users.noreply.github.com>
2022-09-19 13:22:02 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    console.log('client id - '+ socket.id);
+    // send message to all
+    io.emit('chat message', msg);
+
+    // send message to only client id
+    //io.to(socket.id).emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});

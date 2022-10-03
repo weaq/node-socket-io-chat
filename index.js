983ba01b2079d6bf224024ff038764c17e338bf7
weaq <oooweaqooo@gmail.com>
2022-10-03 09:42:21 +0700

    dotenv install

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
